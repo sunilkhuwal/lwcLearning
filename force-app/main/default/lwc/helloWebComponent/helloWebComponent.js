c1aa0f259008aa182a835fba47183a7be9ac7e54
Sunil Kumar <sunil.khuwal@gmail.com>
2021-10-27 13:09:25 +0530

    component information added

--- force-app/main/default/lwc/helloWebComponent/helloWebComponent.js
+++ force-app/main/default/lwc/helloWebComponent/helloWebComponent.js
@@ -1,14 +1,16 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, track, api } from "lwc";
 
 export default class HelloWebComponent extends LightningElement {
-	@track greeting = 'Trailblazer';
+  @track greeting = "Trailblazer";
+  @api strRecordId;
+  arrayFields = ["Name", "AccountNumber", "Phone", "Type", "Website"];
 
-	handleGreetingChange(event){
-		this.greeting = event.target.value;
-	}
+  handleGreetingChange(event) {
+    this.greeting = event.target.value;
+  }
 
-	currentDate = new Date().toDateString();
-	get capitalizedGreeting(){
-		return `Hello ${this.greeting.toUpperCase()}!`;
-	}
+  currentDate = new Date().toDateString();
+  get capitalizedGreeting() {
+    return `Hello ${this.greeting.toUpperCase()}!`;
+  }
 }
